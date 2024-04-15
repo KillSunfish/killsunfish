41c8e8ad242e268a6f59de45dec51d322d7fe7f7
yeri <rlafl7942@naver.com>
2024-04-15 17:21:31 +0900

    feat: 시작 화면 구현

---
+++ src/client/components/Start.java
@@ -0,0 +1,63 @@
+package client.components;
+
+import client.frame.MainView;
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Start extends JComponent {
+    private JButton btn_signIn;
+    private JButton btn_signUp;
+
+    private Images img_sunfish;
+    private Images img_titleText;
+    private MainView mainView;
+
+    public Start (MainView mainView) {
+        this.mainView = mainView;
+        this.btn_signIn = new JButton("로그인");
+        this.btn_signUp = new JButton("회원가입");
+        this.img_sunfish = new Images("src/client/assets/sunfish_Lv1.png");
+        this.img_titleText = new Images("src/client/assets/titleText.png");
+
+        // signIn btn
+        btn_signIn.setBounds(450, 500, 170, 65);
+        btn_signIn.setBackground(Color.white);
+        btn_signIn.setFont(new Font("",Font.BOLD,30));
+        this.add(btn_signIn);
+        btn_signIn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 로그인 버튼 클릭 시 다음 화면으로 전환
+                mainView.switchToLoggedInView("signIn");
+            }
+        });
+
+        // signUp btn
+        btn_signUp.setBounds(750, 500, 170, 65);
+        btn_signUp.setBackground(Color.white);
+        btn_signUp.setFont(new Font("",Font.BOLD,30));
+        this.add(btn_signUp);
+        btn_signUp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 로그인 버튼 클릭 시 다음 화면으로 전환
+                mainView.switchToLoggedInView("signUp");
+            }
+        });
+
+        // sunfish img
+        img_sunfish.setBounds(250, 255, 300, 280);
+        this.add(img_sunfish);
+
+        // titleText img
+        img_titleText.setBounds(450, 300, 700, 100);
+        this.add(img_titleText);
+
+    }
+
+
+}
