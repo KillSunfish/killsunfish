546bd224402a27d44163587c0243cbef78d46601
chanjin1998 <chanjin@kookmin.ac.kr>
2024-04-16 15:40:17 +0900

    feat: 개복치 덩치 키우기 기능

--- src/client/components/Sunfish.java
+++ src/client/components/Sunfish.java
@@ -1,29 +1,57 @@
 package client.components;
+
 import javax.swing.*;
 import java.awt.*;
-    public class Sunfish extends JLabel {
-        private static final String SUNFISH_IMAGE_PATH = "src/client/assets/sunfish.jpg";
 
-        private static final int SUNFISH_WIDTH = 100; // 새로운 이미지의 너비
-        private static final int SUNFISH_HEIGHT = 100; // 새로운 이미지의 높이
+public class Sunfish extends JLabel {
+    private static final String SUNFISH_IMAGE_PATH = "src/client/assets/sunfish.jpg";
+    private static final String SUNFISH2_IMAGE_PATH = "src/client/assets/sunfish_2.jpg";
+    private static final String SUNFISH3_IMAGE_PATH = "src/client/assets/sunfish_3.jpg";
+    private static final String SUNFISH4_IMAGE_PATH = "src/client/assets/sunfish_4.jpg";
 
-        private ImageIcon sunfishImage;
+    private static final int SUNFISH_WIDTH = 100;
+    private static final int SUNFISH_HEIGHT = 100;
 
-        public Sunfish() {
+    private ImageIcon sunfishImage;
+    private Navbar navbar;
 
-            ImageIcon originalImageIcon = new ImageIcon(SUNFISH_IMAGE_PATH);
-            // 이미지를 새로운 크기로 조정
-            Image originalImage = originalImageIcon.getImage();
-            Image scaledImage = originalImage.getScaledInstance(SUNFISH_WIDTH, SUNFISH_HEIGHT, Image.SCALE_SMOOTH);
-            // Load Sunfish image
-            sunfishImage = new ImageIcon(scaledImage);
+    public Sunfish(Navbar navbar) {
+        this.navbar = navbar; // Navbar 객체를 전달받아 필드에 저장
 
-            // Set Sunfish image as the icon
-            setIcon(sunfishImage);
+        ImageIcon originalImageIcon = new ImageIcon(SUNFISH_IMAGE_PATH);
+        Image originalImage = originalImageIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(SUNFISH_WIDTH, SUNFISH_HEIGHT, Image.SCALE_SMOOTH);
+        sunfishImage = new ImageIcon(scaledImage);
 
-            // Set bounds for Sunfish label to position it
-            int x = (1280 - sunfishImage.getIconWidth()) / 2; // 가로 중앙 정렬
-            int y = (980 - sunfishImage.getIconHeight()) / 2; // 세로 중앙 정렬
-            setBounds(x, y, sunfishImage.getIconWidth(), sunfishImage.getIconHeight());
+        setIcon(sunfishImage);
+
+        int x = (1280 - sunfishImage.getIconWidth()) / 2;
+        int y = (980 - sunfishImage.getIconHeight()) / 2;
+        setBounds(x, y, sunfishImage.getIconWidth(), sunfishImage.getIconHeight());
+
+        Timer timer = new Timer(1000, e -> updateSunfishImage());
+        timer.start();
+    }
+
+    public void updateSunfishImage() {
+        double weight = navbar.getWeight();
+        if (weight < 10.0) {
+            setSunfishImage(SUNFISH_IMAGE_PATH);
+        } else if (weight < 20.0) {
+            setSunfishImage(SUNFISH2_IMAGE_PATH);
+        } else if (weight < 30.0) {
+            setSunfishImage(SUNFISH3_IMAGE_PATH);
+        } else {
+            setSunfishImage(SUNFISH4_IMAGE_PATH);
         }
     }
+
+    private void setSunfishImage(String imagePath) {
+        ImageIcon originalImageIcon = new ImageIcon(imagePath);
+        Image originalImage = originalImageIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(SUNFISH_WIDTH, SUNFISH_HEIGHT, Image.SCALE_SMOOTH);
+        sunfishImage = new ImageIcon(scaledImage);
+
+        setIcon(sunfishImage);
+    }
+}
